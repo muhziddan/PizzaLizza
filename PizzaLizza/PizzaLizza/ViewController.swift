0e9c0ec4aa316485788dc5b0e50fba89fd53a7ff
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-11-09 14:49:50 +0700

    Initial commit

---
+++ PizzaLizza/PizzaLizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaLizza
+//
+//  Created by Muhammad Ziddan Hidayatullah on 09/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
