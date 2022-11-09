7739975b2cc9dd4cfc756818574233e0af21cc73
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-11-09 15:33:40 +0700

    Setup navbar appearances

--- PizzaLizza/PizzaLizza/ViewController.swift
+++ PizzaLizza/PizzaLizza/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        let navBarAppearances = UINavigationBarAppearance()
+        navBarAppearances.configureWithOpaqueBackground()
+        navBarAppearances.backgroundColor = UIColor(named: "MainRed")
+        navBarAppearances.titleTextAttributes = [
+            .font: UIFont.boldSystemFont(ofSize: 27),
+            .foregroundColor: UIColor.white
+        ]
+        navigationController?.navigationBar.standardAppearance = navBarAppearances
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearances
+        
+        navigationItem.title = "Pizza Lizza"
     }
 
 
